7203a74adfbd0bfcc2c65af3cf195c88cf650497
Antonio <antoniosimeonov7@gmail.com>
2023-12-20 14:03:17 +0200

    Fix: vite enf interfaces

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,9 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  // Define your environment variables here, for example:
+  // Previously defined environment variables
   readonly VITE_APP_TITLE: string;
-  // ...
+
+  // Add your new environment variables here
+  readonly VITE_NODE_ENV: string;
+  readonly VITE_PAYPAL_CLIENT_ID: string;
+  // ... any other environment variables you use
 }
 
 interface ImportMeta {
