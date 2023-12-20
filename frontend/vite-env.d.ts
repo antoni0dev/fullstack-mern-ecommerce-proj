ee457dc68c06f58f4aa1bd1620bdc41914d64e6e
Antonio <antoniosimeonov7@gmail.com>
2023-12-20 14:00:04 +0200

    Fix:L vite-env file

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // Define your environment variables here, for example:
+  readonly VITE_APP_TITLE: string;
+  // ...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
